dac1cb6d86a47c7a495cd85e6e331e18d1a17cd8
1946258625yXU <1946258625yxu@kuaishou.com>
2020-06-07 21:45:57 +0800

    完成了分页，解决了分页bug

---
+++ ming_OS_3/libc/heap.c
@@ -0,0 +1,1 @@
+#include "../libc/heap.h"
---
+++ ming_OS_3/libc/heap.h
@@ -0,0 +1,16 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+
+#include <stdint.h>
+#include <stddef.h>
+#include "mem.h"
+#include "../cpu/isr.h"
+#include "../drivers/screen.h"
+#include "../libc/string.h"
+#include "../libc/pmm.h"
+#include "../libc/mem.h"
+
+///////////////////////////////////////////
+
+#endif
